148589a3242b25db2a4e0e1af8eb05725508d7c5
Chandrakant <aryalinux11@gmail.com>
2019-10-02 19:31:08 +0530

    Initial commit

---
+++ src/main/java/info/aryalinux/examples/restpostexample/RestPostExampleApplication.java
@@ -0,0 +1,13 @@
+package info.aryalinux.examples.restpostexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPostExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPostExampleApplication.class, args);
+	}
+
+}
